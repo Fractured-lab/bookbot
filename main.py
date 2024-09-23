a719543427a74940a9827e537e67f347c6bd6381
Fractured-lab <fracturedgamess@gmail.com>
2024-09-23 12:06:37 -0400

    added letter counting to main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from collections import Counter
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     words = get_book_word_count(text)
     letters = get_book_letter_count(text)
-    print(text, words)
+    print(text, words, letters)
     
 def get_book_text(path):
     with open(path) as f:
@@ -15,7 +16,13 @@ def get_book_word_count(text):
     return len(words)
 def get_book_letter_count(text):
     lowered_txt = text.lower()
-    
+    letters_only = ""
+    alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for i in lowered_txt:
+        if i in alphabet:
+            letters_only += i
+    return Counter(letters_only)
+        
     
 
 
