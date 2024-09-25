7dd5a33bb209cb73f3fc778d8f46dbdda14866dc
Fractured-lab <fracturedgamess@gmail.com>
2024-09-24 23:52:38 -0400

    Refactored main.py, added report to main()

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from collections import Counter
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     words = get_book_word_count(text)
-    letters = get_book_letter_count(text)
-    print(text, words, letters)
+    chars = get_book_letter_count(text)
+    sorted_chars = get_sorted_letter_count(chars)
+    print(f"---Report of {book_path}---")
+    print(f"{words} total words in document")
+    print()
+    for item in sorted_chars:
+        print(f"The Letter {item['char']} was found {item['num']} times.")
+    print("=====End Report=====")
     
 def get_book_text(path):
     with open(path) as f:
@@ -16,12 +21,34 @@ def get_book_word_count(text):
     return len(words)
 def get_book_letter_count(text):
     lowered_txt = text.lower()
-    letters_only = ""
-    alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    for i in lowered_txt:
-        if i in alphabet:
-            letters_only += i
-    return Counter(letters_only)
+    chars = {}
+    for c in lowered_txt:
+        if c in chars:
+            chars[c] += 1
+        else:
+            chars[c] = 1
+    return chars
+
+def sort_on(d):
+    return d["num"]
+
+def get_sorted_letter_count(chars):
+    letters = []
+    for char in chars:
+        if char.isalpha():
+            #print(char)
+            key_value_pair = {"char": char, "num": chars[char]}
+            #print(key_value_pair)
+            letters.append(key_value_pair)
+            #print(letters)
+    letters.sort(reverse=True, key=sort_on)
+    return letters
+
+
+    
+
+
+
         
     
 
